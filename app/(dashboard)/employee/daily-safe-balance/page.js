8f5849f1a68e7412d235628aa29fee334a5a2117
Ayush021 <12114021@nitkkr.ac.in>
2025-01-23 17:31:51 +0530

    committed

--- app/(dashboard)/employee/daily-safe-balance/page.js
+++ app/(dashboard)/employee/daily-safe-balance/page.js
@@ -8,10 +8,10 @@ export default function DailySafeBalance() {
   return (
     <div className="p-8 h-screen w-screen flex flex-col items-center space-y-4 ">
       <div className="flex w-full lg:w-1/2 items-center space-x-2 ">
-        <Button className="bg-white text-black border border-gray-300 hover:bg-gray-300 " varient="outline">
-          Filter <ListFilter />
+        <Button className="bg-white text-xl text-black border border-gray-300 hover:bg-gray-300 " varient="outline">
+          Filter   <ListFilter  className="!size-6"/>
         </Button>
-        
+      
         <div className="relative w-full">
           <Input type="search" className="bg-white text-black border border-gray-300" placeholder="Search for keyword" />
           <div className="absolute bg-white m-1 inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
@@ -19,8 +19,8 @@ export default function DailySafeBalance() {
           </div>
         </div>
         
-        <Button className="bg-white text-black border border-gray-300 hover:bg-gray-300 " varient="outline">
-          Sort <ArrowDownWideNarrow />
+        <Button className="bg-white text-xl text-black border border-gray-300 hover:bg-gray-300 " varient="outline">
+          Sort <ArrowDownWideNarrow  className="!size-6"/>
         </Button>
       
       </div>
