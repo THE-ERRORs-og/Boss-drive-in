6a98cce2ad6fa76acf37ef694c5c3c70b2fe04d8
Govind Ji <jigovind556@gmail.com>
2025-01-23 19:13:10 +0530

    modified squared button

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,24 +1,27 @@
-"use client";
 import SquareButton from "@/components/Button/SquareButton";
 import admin_login from "@/public/admin_login.svg";
 import staff_login from "@/public/staff_login.svg";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { use } from "react";
 
 export default function Home() {
-  const router = useRouter();
 
  
   return (
     <div className="flex h-screen justify-center items-center">
-        {/* Login Options */}
+      {/* Login Options */}
       <div className="flex justify-center items-center gap-12">
         {/* Admin Login */}
-        <SquareButton text="Login as Admin" imageUrl ={admin_login} onClick={()=>router.push("/Login/admin")} />
+        <SquareButton
+          text="Login as Admin"
+          imageUrl={admin_login}
+          redirectUrl="/Login/admin"
+        />
 
         {/* Staff Login */}
-        <SquareButton text="Login as Staff" imageUrl ={staff_login} onClick={()=>router.push("/Login/employee")}/>
+        <SquareButton
+          text="Login as Staff"
+          imageUrl={staff_login}
+          redirectUrl="/Login/employee"
+        />
       </div>
     </div>
   );
