d8eafaf37cfc0cc7b9865509c1e129512d790dba
Ayush021 <12114021@nitkkr.ac.in>
2025-01-24 00:55:20 +0530

    Changes

--- app/(dashboard)/admin/page.js
+++ app/(dashboard)/admin/page.js
@@ -1,7 +1,32 @@
+import SquareButton from "@/components/Button/SquareButton";
+import staff_management from "@/public/staff_management.svg";
+import daily_safe_balance_history from "@/public/daily_safe_balance_history.svg";
+import employee_order_history from "@/public/employee_order_history.svg";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Route: /admin</h1>
+    <div className="flex h-screen justify-center items-center">
+      {/* Login Options */}
+      <div className="flex justify-center items-center gap-12">
+        {/* Admin Login */}
+        <SquareButton
+          text="Staff Management"
+          imageUrl={staff_management}
+          redirectUrl="/Login/admin"
+        />
+
+        {/* Staff Login */}
+        <SquareButton
+          text="Daily Safe Balance History"
+          imageUrl={daily_safe_balance_history}
+          redirectUrl="/Login/employee"
+        />
+        <SquareButton
+          text="Employee Order History"
+          imageUrl={employee_order_history}
+          redirectUrl="/Login/employee"
+        />
+      </div>
     </div>
   );
 }
--- components/Textbox/StaticDataBox.js
+++ components/Textbox/StaticDataBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const StaticDataBox = ({text , className=""} )=> {
   return (
-    <p className={`${className} md:text-xl pt-2 pl-2 pb-2 md:w-1/6 md:pr-16 border border-black rounded-xl font-semibold `}> {text} </p>
+    <p className={`${className} md:text-xl pt-2 pl-2 pb-2 md:pr-16 border border-black rounded-xl font-semibold `}> {text} </p>
   )
 }
 
