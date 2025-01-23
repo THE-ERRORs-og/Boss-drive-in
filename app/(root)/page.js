c446b0ce027337c14d7e48030ee06e690e48b38d
Ayush021 <12114021@nitkkr.ac.in>
2025-01-23 17:59:34 +0530

    login done

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,27 +1,24 @@
-import admin_login from "../../public/admin_login.svg";
-import staff_login from "../../public/staff_login.svg";
+"use client";
+import SquareButton from "@/components/Button/SquareButton";
+import admin_login from "@/public/admin_login.svg";
+import staff_login from "@/public/staff_login.svg";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { use } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+ 
   return (
     <div className="flex h-screen justify-center items-center">
         {/* Login Options */}
       <div className="flex justify-center items-center gap-12">
         {/* Admin Login */}
-        <div className="flex flex-col items-center border-2 border-red-500 rounded-lg p-10 hover:scale-105 transition transform duration-300">
-          <Image src={admin_login} alt="Admin Icon" className="w-20 h-20 mb-4" />
-          <button className="text-lg font-semibold hover:text-red-500">
-            Login as Admin
-          </button>
-        </div>
+        <SquareButton text="Login as Admin" imageUrl ={admin_login} onClick={()=>router.push("/Login/admin")} />
 
         {/* Staff Login */}
-        <div className="flex flex-col items-center border-2 border-red-500 rounded-lg p-10 hover:scale-105 transition transform duration-300">
-          <Image src={staff_login} alt="Staff Icon" className="w-20 h-20 mb-4" />
-          <button className="text-lg font-semibold hover:text-red-500">
-            Login as Staff
-          </button>
-        </div>
+        <SquareButton text="Login as Staff" imageUrl ={staff_login} onClick={()=>router.push("/Login/employee")}/>
       </div>
     </div>
   );
