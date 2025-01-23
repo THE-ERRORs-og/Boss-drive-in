2e567aeb504229c751d1af6df762c11a5ff5ea5c
Govind Ji <jigovind556@gmail.com>
2025-01-23 16:59:20 +0530

    resolved conflict in login page of shivam branch

--- app/(root)/Login/page.js
+++ app/(root)/Login/page.js
@@ -1,13 +1,13 @@
 "use client";
-// import { auth, signIn, signOut } from "@/auth";
-// import { doCredentialLogin } from "@/lib/actions/authentication";
-// import { useRouter } from "next/navigation";
+import { auth, signIn, signOut } from "@/auth";
+import { doCredentialLogin } from "@/lib/actions/authentication";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import arrow_back from "../../../public/arrow_back.svg";
 import staff_illu from "../../../public/staff_illu.svg";
 
 export default function Page() {
-  // const router = useRouter();
+  const router = useRouter();
 
   const handleFormSubmit = async (event) => {
     event.preventDefault(); // Prevent the default form submission behavior
@@ -19,38 +19,19 @@ export default function Page() {
       }
       const response = await doCredentialLogin(formValues);
 
-  //     if (response.success) {
-  //       console.log("User logged in successfully");
-  //       router.push("/admin");
-  //     }
-  //     else {
-  //       console.error("Error:", response.error);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //   }
-  // };
+      if (response.success) {
+        console.log("User logged in successfully");
+        router.push("/admin");
+      }
+      else {
+        console.error("Error:", response.error);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
-    // <div>
-    //   <h1>Route: /Login</h1>
-    //   <form
-    //   //  onSubmit={handleFormSubmit}
-    //    >
-    //     {" "}
-    //     {/* Use `onSubmit` instead of `action` */}
-    //     <label>
-    //       Email
-    //       <input name="email" type="email" required />
-    //     </label>
-    //     <label>
-    //       Password
-    //       <input name="password" type="password" required />
-    //     </label>
-    //     <button type="submit">Sign In</button>{" "}
-    //     {/* Explicitly set `type="submit"` */}
-    //   </form>
-    // </div>
     <div className="h-screen flex flex-col">
     {/* Content */}
     <div className="flex flex-1 items-center justify-between px-10">
@@ -67,13 +48,14 @@ export default function Page() {
         </h1>
 
         {/* Form */}
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleFormSubmit}>
           {/* Employee ID */}
           <div>
             <label className="block text-sm font-medium text-gray-700">
               Enter employee ID <span className="text-red-500">*</span>
             </label>
             <input
+              name = "userid"
               type="text"
               placeholder="Example: 12345678"
               className="mt-1 w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500"
@@ -86,6 +68,7 @@ export default function Page() {
               Enter your password <span className="text-red-500">*</span>
             </label>
             <input
+              name = "password"
               type="password"
               placeholder="Example: 12345678"
               className="mt-1 w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500"
