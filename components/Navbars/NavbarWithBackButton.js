8c14a26d3d3e4579c9e0324a19f029812a6350ff
Ayush021 <12114021@nitkkr.ac.in>
2025-02-02 19:52:23 +0530

    Add Daily Cash Flow and Employee Order History components with pagination and search functionality

--- components/Navbars/NavbarWithBackButton.js
+++ components/Navbars/NavbarWithBackButton.js
@@ -1,22 +1,27 @@
 import { auth, signOut } from "@/auth";
 import BackButton from "../Button/BackButton";
+import { LogOut } from "lucide-react";
 
 export default async function NavbarWithBackButton() {
   const session = await auth();
 
   return (
-    <nav className="h-16 w-full bg-white flex justify-between items-center px-4 py-4 shadow-md">
-      <BackButton/>
+    <nav className="h-20 w-full bg-white flex justify-between items-center px-8 py-8 ">
+      <BackButton />
       <img
         src="/logo.png" // Replace this with your logo's path
         alt="Boss Drive-In"
-        className="h-10 mx-auto"
+        className="h-12 mx-auto"
       />
       {session && session.user && (
-        <button onClick={async ()=>{
-          'use server';
-          await signOut({redirectTo :"/"});
-        }}>Logout</button>)}
+        <LogOut
+        size={48}  strokeWidth={1}
+          onClick={async () => {
+            "use server";
+            await signOut({ redirectTo: "/" });
+          }}
+        />
+      )}
     </nav>
   );
 }
