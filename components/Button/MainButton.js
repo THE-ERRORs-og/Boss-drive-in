35ccb237e940faabcf23f833eefb10c3f80c0140
Govind Ji <jigovind556@gmail.com>
2025-02-02 23:19:49 +0530

    toast in login page

--- components/Button/MainButton.js
+++ components/Button/MainButton.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const MainButton = ({text ,className="" , handleFormSubmit}) => {
+const MainButton = ({
+  text,
+  className = "",
+  isLoading = false,
+  handleFormSubmit,
+}) => {
   return (
     <button
-    type="submit"
-    onSubmit={handleFormSubmit}
-    className={` ${className} p-10 bg-[#ED1C24] text-white text:sm font-semibold py-2 border border-gray-400 rounded-lg hover:bg-white hover:text-[#ED1C24] transition duration-300 ease-in-out`}
-  >
-   {text}
-  </button>
-  )
-}
+      type="submit"
+      onSubmit={handleFormSubmit}
+      disabled={isLoading}
+      className={`p-10 text-white text:sm font-semibold py-2 border border-gray-400 rounded-lg transition duration-300 ease-in-out ${className}
+      ${
+        isLoading
+          ? "bg-gray-400 cursor-not-allowed"
+          : "bg-red-500 hover:bg-red-600"
+      }`}
+    >
+      {text}
+    </button>
+  );
+};
 
-export default MainButton
+export default MainButton;
